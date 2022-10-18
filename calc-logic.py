28f12110356f5df5ee471936d1b90b78b99f4d85
Aniruddha Ghosh <100751330+DMMPrice@users.noreply.github.com>
2022-10-18 10:50:27 +0530

    Ani (#2)
    * Working with GUI
    
    * Basic GUI and model ready

--- calc-logic.py
+++ calc-logic.py
@@ -4,23 +4,48 @@ import operator
 r = s_r.Recognizer()
 my_mic_device = s_r.Microphone(device_index=1)
 
+
 # print all the microphones connected to your machine
 # print(s_r.Microphone.list_microphone_names())
+def operator_re(test_string):
+    if test_string.find('x') > 0:
+        test_string.replace("x", "*")
+        return test_string
+    if test_string.find('to the power') > 0:
+        test_string.replace("to the power", "*")
+        return test_string
+    if test_string.find('into') > 0:
+        test_string.replace("into", "*")
+        return test_string
+    else:
+        return test_string
+
 
 with my_mic_device as source:
     print("Say what you want to calculate, example: 3 plus 3")
-    r.adjust_for_ambient_noise(source)
+    r.adjust_for_ambient_noise(source, duration=0.5)
     audio = r.listen(source)
-my_string = r.recognize_google(audio)
-print(my_string)
+    my_string = r.recognize_google(audio)
+    # print(type(my_string))
 
+my_string = my_string.lower()
+
+
+# print(my_string)
+# print(my_string.replace("into", "*"))
+# print('The changed string is')
+# changed_str = operator_re(my_string)
+# print(changed_str)
 
 def get_operator_fn(op):
     return {
         '+': operator.add,
         '-': operator.sub,
+        'into': operator.mul,
+        'multiplied by': operator.mul,
         'x': operator.mul,
         'divided': operator.__truediv__,
+        'by': operator.__truediv__,
         'Mod': operator.mod,
         'mod': operator.mod,
         '^': operator.xor,
